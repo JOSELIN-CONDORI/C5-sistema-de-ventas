835a90d19d13ea1770d982b3e061fb34f3db3ed7
JOSELIN-CONDORI <yoselin.tintaya.25@gmail.com>
2023-09-24 09:13:22 -0500

    proyecto examen

---
+++ src/main/java/com/example/c5sistemadeventas/C5SistemaDeVentasApplication.java
@@ -0,0 +1,13 @@
+package com.example.c5sistemadeventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C5SistemaDeVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C5SistemaDeVentasApplication.class, args);
+	}
+
+}
